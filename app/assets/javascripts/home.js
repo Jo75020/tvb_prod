bb1365b684992c981161f06ddd56faa8112e76a5
Jordan Demmouche <jordand75020@hotmail.fr>
2018-11-21 23:50:54 +0100

    refacto

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,30 +3,38 @@ $(window).scroll(function() {
   var scroll_position = $(window).scrollTop()
   var w_logo = document.getElementById("white-logo")
   var b_logo = document.getElementById("div-logo-black")
+  var mid_text = document.getElementById("text-middle")
 
     if ( scroll_position >= sc &&  w_logo.style.opacity != '0'  ) {
       w_logo.style.opacity = '0';
       w_logo.style.transition = '0.6s';
-      b_logo.style.opacity = '0.8';
+      b_logo.style.opacity = '0.5';
+      mid_text.style.opacity = '1';
     }
     if ( scroll_position <= sc &&  w_logo.style.opacity == '0' && scroll_position <= 1500 ) {
       w_logo.style.opacity = '1';
       b_logo.style.opacity = '0';
+      mid_text.style.opacity = '0';
     }
     if ( scroll_position >= 1051  ) {
       w_logo.style.opacity = '1';
       w_logo.style.transition = '0.6s';
       b_logo.style.opacity = '0';
+      mid_text.style.opacity = '0';
     }
 });
 
  $(function() {
-    $('#go-to-us').click (function() {
+    $('#go-us').click (function() {
       $("html, body").animate({ scrollTop: 700 }, 600);
       return false;
     });
-    $('#go-to-event').click (function() {
+    $('#go-event').click (function() {
       $("html, body").animate({ scrollTop: 1670 }, 600);
+      return false;
+    });
+    $('#go-up').click (function() {
+      $("html, body").animate({ scrollTop: 0 }, 600);
       return false;
     });
   });
